1271b98f8c1e774c1f160f715edd7b96ed1e1c67
arbinzaman <arbinzmn@gmail.com>
2022-09-03 21:13:12 +0600

    finised most off the work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,145 @@
-console.log('hello')
+// dynamic Catagories to Show Step 1
+const loadCategories = async () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      displayCategories(data.data.news_category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+const displayCategories = categories =>{
+    // console.log(categories);
+   
+    
+    categories.forEach(categorie => {
+    const catagoriesList = document.getElementById('catagories-list');
+    const categoriesDiv = document.createElement('div');
+    // console.log(categorie.category_name);
+    categoriesDiv.classList.add('w-50');
+    categoriesDiv.classList.add('btn');
+    categoriesDiv.innerHTML= `
+    <button onclick="loadNews(${categorie.category_id})" class="btn  btn-success">${categorie.category_name}</button>
+    `
+   
+    catagoriesList.appendChild(categoriesDiv);
+     } )};
+    loadCategories();
+    // display no news found
+
+// spinner
+    const toggoleSpinner = (isLoading) => {
+        const loaderSection = document.getElementById("loader");
+        if (isLoading) {
+          loaderSection.classList.remove("d-none");
+        } else {
+          loaderSection.classList.add("d-none");
+        }
+      };
+
+
+// News Details to Show Step 2
+    const loadNews = async (id) => {
+        toggoleSpinner(true);
+        const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
+        try {
+          const res = await fetch(url);
+          const data = await res.json();
+          displayNews(data.data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+         
+
+ const noNewsFound = document.getElementById('no-news-found');
+    const displayNews = newses =>{
+        // console.log(newses);
+     
+       
+        if(newses.length === 0){
+            noNewsFound.classList.remove('d-none')
+        }
+        else{
+            noNewsFound.classList.add('d-none')
+        }
+
+
+        const newsList = document.getElementById('news-list');
+        newsList.innerHTML='';
+        newses.forEach(news => {
+        // console.log(newses.length);
+    
+
+    //   number of news
+        const numberOfNewsFound = document.getElementById('number-of-news');
+        numberOfNewsFound.innerHTML='';
+        const newsNumberDiv = document.createElement('div');
+        newsNumberDiv.classList.add('text-center');
+        newsNumberDiv.innerHTML=`
+        <h1>${newses.length} News Found</h1>
+        `;
+        numberOfNewsFound.appendChild(newsNumberDiv) ;
+
+
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col-md-4');
+        newsDiv.innerHTML=`
+        <div class="card">
+            <img class="card-img-top" src="${news.image_url}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${news.title}</h5>
+              <p class="card-text">${news.details.length > 100 ? news.details.slice(0, 150) + "...": details}</p>
+              <img class="rounded-circle me-4" src="${news.author.img}" alt="" width="30" height="30">
+              <p class="card-text"><small class="text-muted">${news.author.name} </small></p>
+              <p>Total Views : ${news.total_view}</P>
+              <button onclick="loadNewsDetails('${news._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+            </div>
+        </div>    
+        `;
+        newsList.appendChild(newsDiv) ;
+    } );
+    toggoleSpinner(false);
+};
+        
+
+
+    loadNews();
+    const loadNewsDetails = async (id) => {
+        const url =  `https://openapi.programming-hero.com/api/news/${id}`;
+        try {
+          const res = await fetch(url);
+          const data = await res.json();
+          displayNewsDetails(data.data[0]);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      
+      const displayNewsDetails = (details) => {
+        // console.log(details);
+        const modalTitle = document.getElementById("exampleModalLabel");
+        modalTitle.innerText = details.title;
+        const detailImg = document.getElementById("detail-img");
+        detailImg.innerHTML = `
+        <img src="${details.author.img}" class="img-fluid">
+        <h5>Author Name:${details.author.name ? details.author.name : "No Name Available"}</h5>
+        <p>Publish Date:${details.author.published_date}</p>
+        <p>Total View :${details.total_view ? details.total_view : "No data found"}<p>
+        `;
+      };
+
+      loadNewsDetails();
+
+
+// // display no news found
+//   const noNewsFound = document.getElementById('no-news-found');
+//   if(phone.length === 0){
+//       noNewsFound.classList.remove('d-none')
+//   }
+//   else{
+//       noNewsFound.classList.add('d-none')
+//   }
